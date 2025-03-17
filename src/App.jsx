454c2024c09886537da136d22782a4e41c182d97
Aditya Vaishnav <aditya.vaishnav@mercanis.com>
2025-03-17 12:28:17 +0530

    add new functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 import React, { useState } from 'react'
-let queue = Promise.resolve();
+// let queue = Promise.resolve();
 
 const App = () => {
 
 const [todos, setTodos ] = useState([]);
+const [pendingDelay, setPendingDelay] = useState(0);
 
  const addTodo = (delay = 0) => {
-  queue = queue.then(() => 
-    new Promise((resolve)=> {
       const id = Date.now();
       setTodos((prev) => [...prev, {id, text: "Loading..."}]
     );
 
+    const newDelay = pendingDelay + delay;
+    setPendingDelay(newDelay);
+
  setTimeout(()=> {
   setTodos((prev) => prev.map((todo)=> 
-  todo.id === id ? {id, text: `Todo - ${id}`} : todo
-  ))
-  resolve();
- }, delay)
+  (todo.id === id ? {id, text: `Todo - ${id}`} : todo
+  )));
+  setPendingDelay((current) => (current === newDelay ? 0 : current - delay ));
 
-    })
-  )
+ }, newDelay);  
  }
 
 
   return (
-    <div className='flex h-screen p-4 bg-gray-100'>
+    <div className='flex flex-row h-screen p-4 bg-gray-100'>
       {/* Left side  */}
-      <div className='flex-1 bg-white shadow p-4 rounded-lg overflow-auto'>
+      <div className='w-1/2 flex-1 bg-white shadow p-4 rounded-lg overflow-auto'>
 <h2 className='text-xl font-semibold mb-2'>Todo List</h2>
 <div className='space-y-2 max-h-[80vh] overflow-auto'>
   {
@@ -43,7 +43,7 @@ const [todos, setTodos ] = useState([]);
       </div>
 
       {/* Right side */}
-      <div className='w-1/2 ml-4 flex flex-col justify-center space-y-2'>
+      <div className='w-1/2 ml-4 flex flex-col justify-center space-y-2 md:mb-3'>
         <button onClick={() => addTodo(0)} className=' p-2 bg-gray-50 text-black rounded-lg shadow border-2 w-1/2'>Add Todo</button>
         <button onClick={() => addTodo(2000)} className='p-2 bg-gray-50 text-black  rounded-lg shadow border-2 w-1/2'>Add After 2s</button>
         <button onClick={()=> addTodo(5000)} className='p-2 bg-gray-50 text-black  rounded-lg shadow border-2 w-1/2'>Add After 5s</button>
